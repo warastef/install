1d767de704fab4a5ce1dad58e0d70a086fa84348
steffert1 <135466860+warastef@users.noreply.github.com>
2023-06-11 22:31:20 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,18 +1,3 @@
-# get latest download url
-$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
-        Select-Object -ExpandProperty "assets" |
-        Where-Object "browser_download_url" -Match '.msixbundle' |
-        Select-Object -ExpandProperty "browser_download_url"
-
-# download
-Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
-
-# install
-Add-AppxPackage -Path "Setup.msix"
-
-# delete file
-Remove-Item "Setup.msix"
-
-
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $env:TEMP\winget.msixbundle
+Add-AppxPackage -Path $env:TEMP\winget.msixbundle
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/warastef/install/main/install.ps1'))
